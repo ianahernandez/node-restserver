d88381e9da1d9a6d4575829cc4c2ea10f7de85a7
Ana <anaceciliah95@gmail.com>
2019-03-25 11:29:21 -0400

    Variable de entorno mongo

--- server/config/config.js
+++ server/config/config.js
@@ -20,6 +20,9 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://172.17.0.2:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://ianahernandez:EPnOKdyrT8NjeWba@cluster0-kheys.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URI;
 }
 process.env.URLDB = urlDB;
+
+
+// MONGO_URI='mongodb+srv://ianahernandez:EPnOKdyrT8NjeWba@cluster0-kheys.mongodb.net/cafe' en heroku
